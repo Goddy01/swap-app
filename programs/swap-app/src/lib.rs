d2704fc58339e1ffaeb9fbeb9c7f97af1662a49b
Goddy01 <adigungodwin2@gmail.com>
2024-12-10 16:15:13 +0100

    updated lib.rs

--- programs/swap-app/src/lib.rs
+++ programs/swap-app/src/lib.rs
@@ -15,8 +15,8 @@ declare_id!("35B6fNAgPfqeW9d9qANsQrECSYFDxoobTo3CTrZJxAvJ");
 pub mod swap_app {
     use super::*;
 
-    pub fn make_offer(ctx: Context<MakeOffer>) -> Result<()> {
-        instructions::make_offer::send_offered_tokens_to_vault()?;
-        instructions::make_offer::save_offer()
+    pub fn make_offer(ctx: Context<MakeOffer>, id:u64, token_a_offered_amount: u64, token_b_offered_amount: u64) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_offered_amount);
     }
 }
